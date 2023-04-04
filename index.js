844c4c90e3e159012e060f3eb123a13656f2e49c
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-05 00:01:46 +0530

    Updated index.ejs

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+//setting up view engine
+app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
     // res.send("Welcome to express");
-    const pathlocation = path.resolve();
-    res.sendFile(path.join(pathlocation, './index.html'));
+    // const pathlocation = path.resolve();
+    // res.sendFile(path.join(pathlocation, './index.html'));
+
+    // res.render('index.ejs');
+    res.render('index', {name: 'Eshwar'});
+
 })
 
 app.listen(5000, () => {
