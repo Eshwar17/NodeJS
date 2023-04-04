0310bc743f440bbe8a1fe5aa77b0f2decf113764
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-04 20:53:43 +0530

    Created server and listened to it using port 5000

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello I am NodeJs");
+const http = require('http');
+
+const server = http.createServer(() => {
+    console.log("I am Create Server function")
+});
+
+server.listen(5000,() => {
+    console.log("Server is working");
+});
+
