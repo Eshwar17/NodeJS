2b2b44d1ff405a14e1fbd81e12a155b4349efbd6
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-05 00:57:09 +0530

    Added middleware app.use(express.urlencoded({extended: true}))

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+
+//static files
+app.use(express.static(path.join(path.resolve(),'public')));
+//middleware to see data
+app.use(express.urlencoded({extended: true}));
+
 //setting up view engine
 app.set("view engine","ejs");
 
@@ -13,6 +19,10 @@ app.get('/', (req, res) => {
     // res.render('index.ejs');
     res.render('index', {name: 'Eshwar'});
 
+})
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send("Success");
 })
 
 app.listen(5000, () => {
