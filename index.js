fa7237908d0adb36fd8bb1a9443cc623619a89cd
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-04 23:19:20 +0530

    Created server in express using express() function

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-// const http = require('http');
-// const gfName = require('./feature')
-import http from 'http'
-import gfName, { gfName1, gfName2 } from './feature.js'
-import fs from 'fs'
+import express from 'express';
 
-console.log(gfName);
-console.log(gfName1);
-console.log(gfName2);
+const server = express();
 
-const home = fs.readFileSync("./index.html");
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/about') {
-        res.end('<h1>About page</h1>');
-    }
-    else if(req.url === '/contact'){
-        res.end('<h1>Contact page</h>');
-    }
-    else if(req.url === '/') {
-        // fs.readFile("./index.html", function(err, data){
-        //     res.end(data);
-        // })
-        res.end(home);
-    }
-    else{
-        res.end('<h1>Page not found</h1>');
-    }
+server.listen(5000, () => {
+    console.log('listening on port');
 });
-
-server.listen(5000,() => {
-    console.log("Server is working");
-});
-
